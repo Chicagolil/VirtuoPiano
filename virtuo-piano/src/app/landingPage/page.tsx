d02b0688b64cf46886f8cc4d8bc012473b69a86a
Chicagolil <he202305@students.ephec.be>
2025-04-05 23:12:46 +0200

    Cleaning V2

--- virtuo-piano/src/app/landingPage/page.tsx
+++ virtuo-piano/src/app/landingPage/page.tsx
@@ -7,16 +7,11 @@ import Image from 'next/image';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
-import PixelCanvas from '@/components/PixelCanvas';
 
 export default function LandingPage() {
   return (
     <div className="pixel-background">
-      <PixelCanvas
-        gap={8}
-        speed={40}
-        colors="#f8fafc, #f1f5f9, #cbd5e1, #94a3b8"
-      />
+      <h1>Hello</h1>
     </div>
   );
 }
