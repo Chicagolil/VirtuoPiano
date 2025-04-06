1c2604ba454c84989f5a557a2dba82f1ac46c31a
Chicagolil <he202305@students.ephec.be>
2025-04-06 22:26:15 +0200

    Test

--- virtuo-piano/src/app/page.tsx
+++ virtuo-piano/src/app/page.tsx
@@ -3,7 +3,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/authoption';
 
 import LandingPage from './landingPage/page';
-
+// vercel redeploy
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
