850031269563e8c6edb114453f1ad072b0e91c67
Chicagolil <he202305@students.ephec.be>
2025-04-11 22:27:17 +0200

    faut arrêter les minis commit là

--- virtuo-piano/src/app/layout.tsx
+++ virtuo-piano/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import AnimatedBackground from '@/components/AnimatedBackground';
 import { AuroraBackground } from '@/components/ui/aurora-background';
 
 const inter = Inter({ subsets: ['latin'] });
