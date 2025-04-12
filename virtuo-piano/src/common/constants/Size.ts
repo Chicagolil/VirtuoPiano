1d9dfa5bfa0d94d53fb18ce49e44dc2fb986dea1
Chicagolil <he202305@students.ephec.be>
2025-04-13 01:42:14 +0200

    Papapapaaa on a des icônes en saaaaah et le boutton de signout Comment il est boooo

---
+++ virtuo-piano/src/common/constants/Size.ts
@@ -0,0 +1,8 @@
+export enum Size {
+  XS = 16,
+  S = 28,
+  M = 34,
+  L = 38,
+  XL = 56,
+  XXL = 72,
+}
--- virtuo-piano/src/features/layout/Navbar.tsx
+++ virtuo-piano/src/features/layout/Navbar.tsx
@@ -3,13 +3,19 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import styles from './Navbar.module.css';
-import LordIcon, { FavoriteHeart } from '../../components/LordIcon';
-
-// Dans votre composant :
+import LottieIcon, { LottieIconHandle } from '@/components/ui/LottieIcon';
+import { useRef } from 'react';
+import SignOutButton from '@/components/SignOutButton';
+import { Size } from '@/common/constants/Size';
 
 export default function Navbar() {
   const pathname = usePathname();
-
+  const favoritesIconRef = useRef<LottieIconHandle>(null);
+  const performancesIconRef = useRef<LottieIconHandle>(null);
+  const importsIconRef = useRef<LottieIconHandle>(null);
+  const libraryIconRef = useRef<LottieIconHandle>(null);
+  const leaderboardIconRef = useRef<LottieIconHandle>(null);
+  const iconSize = Size.M;
   return (
     <nav className={styles.nav}>
       <div className={styles.container}>
@@ -18,64 +24,123 @@ export default function Navbar() {
         </div>
 
         <ul className={styles.menu}>
-          <li className={pathname === '/library' ? styles.active : ''}>
+          <li
+            className={pathname === '/library' ? styles.active : ''}
+            onMouseEnter={() => libraryIconRef.current?.replay()}
+          >
             <Link href="/library">
               <span
-                className={`${styles.menuItem} ${
+                className={`${styles.menuItem} ${styles.menuItemWithIcon} ${
                   pathname === '/library' ? styles.active : ''
                 }`}
               >
+                <LottieIcon
+                  ref={libraryIconRef}
+                  src="/icons/library.json"
+                  loop={false}
+                  autoplay={false}
+                  width={iconSize}
+                  height={iconSize}
+                />
                 Librairie
               </span>
             </Link>
           </li>
 
-          <li className={pathname === '/favorites' ? styles.active : ''}>
+          <li
+            className={pathname === '/favorites' ? styles.active : ''}
+            onMouseEnter={() => favoritesIconRef.current?.play()}
+            onMouseLeave={() => favoritesIconRef.current?.playReverse()}
+          >
             <Link href="/favorites">
               <span
-                className={`${styles.menuItem} ${
+                className={`${styles.menuItem} ${styles.menuItemWithIcon} ${
                   pathname === '/favorites' ? styles.active : ''
                 }`}
               >
+                <LottieIcon
+                  ref={favoritesIconRef}
+                  src="/icons/favorites.json"
+                  loop={false}
+                  autoplay={false}
+                  width={iconSize}
+                  height={iconSize}
+                />
                 Favoris
               </span>
             </Link>
           </li>
-          <li className={pathname === '/performances' ? styles.active : ''}>
+          <li
+            className={pathname === '/performances' ? styles.active : ''}
+            onMouseEnter={() => performancesIconRef.current?.play()}
+            onMouseLeave={() => performancesIconRef.current?.playReverse()}
+          >
             <Link href="/performances">
               <span
-                className={`${styles.menuItem} ${
+                className={`${styles.menuItem} ${styles.menuItemWithIcon} ${
                   pathname === '/performances' ? styles.active : ''
                 }`}
               >
+                <LottieIcon
+                  ref={performancesIconRef}
+                  src="/icons/performances.json"
+                  loop={false}
+                  autoplay={false}
+                  width={iconSize}
+                  height={iconSize}
+                />
                 Performances
               </span>
             </Link>
           </li>
-          <li className={pathname === '/leaderboard' ? styles.active : ''}>
+          <li
+            className={pathname === '/leaderboard' ? styles.active : ''}
+            onMouseEnter={() => leaderboardIconRef.current?.replay()}
+          >
             <Link href="/leaderboard">
               <span
-                className={`${styles.menuItem} ${
+                className={`${styles.menuItem} ${styles.menuItemWithIcon} ${
                   pathname === '/leaderboard' ? styles.active : ''
                 }`}
               >
+                <LottieIcon
+                  ref={leaderboardIconRef}
+                  src="/icons/leaderboard2.json"
+                  loop={false}
+                  autoplay={false}
+                  width={iconSize}
+                  height={iconSize}
+                />
                 Classement
               </span>
             </Link>
           </li>
-          <li className={pathname === '/imports' ? styles.active : ''}>
+          <li
+            className={pathname === '/imports' ? styles.active : ''}
+            onMouseEnter={() => importsIconRef.current?.play()}
+            onMouseLeave={() => importsIconRef.current?.playReverse()}
+          >
             <Link href="/imports">
               <span
-                className={`${styles.menuItem} ${
+                className={`${styles.menuItem} ${styles.menuItemWithIcon} ${
                   pathname === '/imports' ? styles.active : ''
                 }`}
               >
+                <LottieIcon
+                  ref={importsIconRef}
+                  src="/icons/mesChansons.json"
+                  loop={false}
+                  autoplay={false}
+                  width={iconSize}
+                  height={iconSize}
+                />
                 Mes Chansons
               </span>
             </Link>
           </li>
         </ul>
       </div>
+      <SignOutButton />
     </nav>
   );
 }
